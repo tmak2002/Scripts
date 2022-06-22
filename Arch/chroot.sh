3fb0d9844b442dc5f6beac905a9545f59cbe01ab
tmak2002 <tmak2002@mailbox.org>
2022-06-22 13:44:03 +0200

    test

--- Arch/chroot.sh
+++ Arch/chroot.sh
@@ -12,4 +12,11 @@ bootctl install
 useradd -m -U -G wheel -s /bin/bash torben
 passwd torben
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+pacman -S xorg-server xorg-xinit \
+  i3-gaps polybar jgmenu \
+  pulseaudio pavucontrol \
+  firefox rxvt-unicode \
+  git \
+
+git clone https://github.com/tmak2002/dotfiles /home/torben
 exit
